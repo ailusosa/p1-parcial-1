9505c45f08c6a4da61b4fcd8295474b68c098fec
Ailén Sosa <ailenjazminsosa@gmail.com>
2025-05-26 14:42:40 -0300

    Add files via upload

---
+++ js/clase-pista.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Pista {
+    #nombre;
+    #duracion;
+
+    constructor(nombre, duracion) {
+        this.#nombre = nombre;
+        this.#duracion = duracion;
+    }
+
+    get getNombre() {
+        return this.#nombre;
+    }
+
+    get getDuracion() {
+        return this.#duracion;
+    }
+
+    /*
+     * Función que genera la estructura html de la pista
+     */
+    generarEstructuraHtml(_clasesExtra = "") {
+      return `
+        <li class="${_clasesExtra}">
+            <h3>${this.#nombre}</h3>
+            <p class="${this.obtenerClases()}">${this.#duracion.toHhMmSs(3)}</p>
+        </li>
+    `;
+    }
+
+    /*
+     * Función que retorna un string con las clases que la
+     * pista debería tener en base a sus características
+     */
+    obtenerClases() {
+        let clases = "duracion";
+        if(this.#duracion > 180) clases += " duracion--larga";
+
+        return clases;
+    }
+
+    get getDuracion() {
+        return this.#duracion;
+    }
+}
